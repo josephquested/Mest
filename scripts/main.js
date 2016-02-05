9787345f62cfddbe53f845c0b0657ce1a3707b97
Joseph Quested <josephquested@icloud.com>
2016-02-05 20:57:12 +1300

    Add npm packages

--- scripts/main.js
+++ scripts/main.js
@@ -1,87 +1,88 @@
-var playerInfo = {
-	"id" : "JJWQ"
+"use strict"
+
+
+
+let playerInfo = {
+	"id" : "JJWQ",
+	"icon" : "†"
 };
 
-function generateTable (id) {
+const cl = (input) => {
+	console.log(input);
+}
+
+const generateTable = (id) => {
 	$("#board").append("<table id=\"" + id + "\"></table>");
 }
 
-function generateTiles (table, exponent) {
-	table = $("#" + table);
-	console.log("I'm gonna create tiles on " + table);
-
+const generateRows = (table, exponent) => {
+	console.log("I'm generating rows!")
 	for (var i = 0; i < exponent; i++) {
-		// generates rows
-		var row = table.append(generateElementAndId("tr", "row_" + i));
+		findObjectById(table).append(generateElementAndId("tr", "row_" + i))
+		console.log("row " , i , " generated.")
+		generateTiles(findObjectById('row_' + i), i, exponent)
+	}
+}
 
-		for (var e = 0; e < exponent; e++) {
-			//generates tiles
-			$("#row_" + String(i)).append(generateElementAndId("td", generateIdString("tile", i , e)));
-			var tile = findTileObjectById(generateIdString("tile", i , e));
+const generateTiles = (rowObj, rowNum, exponent) => {
+	console.log("I'm generating tiles!")
+	for (let i = 0; i < exponent; i++) {
+		rowObj.append(generateElementAndId("td", generateIdString("tile", rowNum, i)));
+		
+		// assigns border classes
+		let tile = findObjectById(generateIdString("tile", rowNum, i))
 
-			// assigns border classes
-			console.log("assigning classed!");
-			if (i === 0) {
-				tile[0].classList.add("northBorder");
-			}
+		if (rowNum === 0) {
+			tile[0].classList.add("northBorder");
+		}
 
-			if (e === exponent - 1) {
-				tile[0].classList.add("eastBorder");
-			}
+		if (i === exponent - 1) {
+			tile[0].classList.add("eastBorder");
+		}
 
-			if (i === exponent - 1) {
-				tile[0].classList.add("southBorder");
-			}
+		if (rowNum === exponent - 1) {
+			tile[0].classList.add("southBorder");
+		}
 
-			if (e === 0) {
-				tile[0].classList.add("westBorder");
-			}
+		if (i === 0) {
+			tile[0].classList.add("westBorder");
 		}
 	}
 }
 
-function generateElementAndId(element, id) {
-	return "<" + element + " id=\"" + id + "\"></" + element + ">";
-}
-
-function generateIdString (element, a, b) {
-	return element + "_" + a + "_" + b;
-}
-
 // ___ PLAYER FUNCTIONS ___
 
-function generatePlayerAtTile(player, tile) {
+const generatePlayerAtTile = (player, tile) => {
 	addPlayerToTile(player, tile);
 }
 
-function getPlayerInfoByName(player) {
+const getPlayerInfoByName = (player) => {
 	//info
 	return player;
 }
 
 // ___ FIND FUNCTIONS ___
 
-function findTileObjectByPlayer(player) {
+const findTileObjectByPlayer = (player) => {
 	var tile = $( "#P_" + player ).parent();
 	return tile;
 }
 
-function findTileObjectById (id) {
+const findObjectById = (id) => {
 	return $("#" + id);
 }
 
-function findElementWithinElement(parentElement, childElement) {
+const findElementWithinElement = (parentElement, childElement) => {
 	
 }
 
-function findPlayerObjectByName(player) {
+const findPlayerObjectByName = (player) => {
 	var playerObject = $( "#P_" + player);
 	return playerObject[0];
 }
 
-function generateNewTableIfNeeded (direction, tableId) {
+const generateNewTableIfNeeded = (direction, tableId) => {
 	var x = 0, y = 0;
-	console.log("generate new table if needed!");
 
 	if (direction == "north") {
 		y = 1;
@@ -100,63 +101,108 @@ function generateNewTableIfNeeded (direction, tableId) {
 	}
 
 	var newId = editIdString(tableId, x, y);
+	var newTableId = findTableIdByDirection(direction, tableId);
 
-	if (checkIfIdExists(newId)) {
-		console.log(newId + " exists!");
+	if (checkIfIdExists(newTableId)) {
+		console.log(newTableId + " exists!")
 
 	} else {
-		generateTable(newId);
-		generateTiles(newId, 6);
-		console.log(newId + " is being created!!");
+		generateTable(newTableId);
+		console.log(newTableId + " is being created!!")
+		generateRows(newTableId, 6);
 	}
-
-	return newId;
 }
 
-function findTileByDirection(currentTile, direction) {
-	var currentTable = currentTile.closest('table')[0];
-	var tableId = generateIdString("table", Number(currentTable.id.split("_")[1]), Number(currentTable.id.split("_")[2]));
-	var row = (Number(currentTile[0].id.split("_")[1]));
-	var tile = (Number(currentTile[0].id.split("_")[2]));
-	console.log("find tile");
+const checkForBorderCrossing = (direction, currentTile) => {
+	if (currentTile.hasClass(direction + 'Border')) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
-	if (currentTile.hasClass("northBorder") || currentTile.hasClass("eastBorder") || currentTile.hasClass("southBorder") || currentTile.hasClass("westBorder")) {
-		console.log("border class working!");
-		var newTable = generateNewTableIfNeeded(direction, tableId);
-		generateNewTableIfNeeded(direction, tableId);
-		console.log($("#" + newTable)) //.children("#" + generateIdString("tile", row, tile))[0]);
-		return $("#" + newTable).children("#" + generateIdString("tile", row, tile))[0];
+const findTileOverBorder = (direction, currentTile) => {
+	var tableId = convertObjectToIdString(currentTile.closest('table'));
+	var tileIdArray = splitIdStringToArray(convertObjectToIdString(currentTile));
+	var row = Number(tileIdArray[1]), tile = Number(tileIdArray[2]);
+
+	cl(tileIdArray)
+}
+
+const findTableByDirection = (direction, currentTableId) => {
+	var tableIdArray = splitIdStringToArray(currentTableId));
+}
+
+const findTableIdByDirection = (direction, tableId) => {
+	var tableIdArray = splitIdStringToArray(tableId);
+	var x = Number(tableIdArray[1]), y = Number(tableIdArray[2]);
+
+	if (direction === "north") {
+		y = 1;
+	}
+		
+	if (direction === "east") {
+		x = 1;
 	}
 
-	if (direction == "north") {
-		row -= 1;
-	}	
-
-	if (direction == "east") {
-		tile += 1;
+	if (direction === "south") {
+		y = -1;
 	}
 
-	if (direction == "south") {
-		row += 1;
+	if (direction === "west") {
+		x = -1;
 	}
 
-	if (direction == "west") {
+	return editIdString(tableId, x, y);
+}
+
+const findTileByDirection = (currentTile, direction) => {
+	var tableId = convertObjectToIdString(currentTile.closest('table'));
+	var tileIdArray = splitIdStringToArray(convertObjectToIdString(currentTile));
+	var row = Number(tileIdArray[1]), tile = Number(tileIdArray[2]);
+
+	if (checkForBorderCrossing(direction, currentTile)) {
+		cl("I am crossing a border")
+		generateNewTableIfNeeded(direction, tableId)
+		return findTileOverBorder(direction, currentTile)
+	}
+
+	if (direction === "north") {
+		row -= 1
+	}
+
+	if (direction === "east") {
+		tile += 1
+	}
+
+	if (direction === "south") {
+		row += 1
+	}
+
+	if (direction === "west") {
 		tile -= 1;
 	}
 
-	console.log("I'm...I'm gonna return!");
-	return $("#" + generateIdString("tile", row, tile))[0];
+	return findObjectById(generateIdString(tileIdArray[0], row, tile))[0];
 }
 
 // ___ MOVE FUNCTIONS ___
 
-function movePlayerToTile(player, tile) {
+const addPlayerToTile = (player, tile) => {
+	tile.innerHTML = "<div id=\"P_" + player + "\"><p>" + getPlayerInfoByName(player) + "</p></div>";
+}
+
+const removePlayerFromTile = (player, tile) => {
+	tile[0].innerHTML = playerInfo.icon;
+}
+
+const movePlayerToTile = (player, tile) => {
 	removePlayerFromTile(player, findTileObjectByPlayer(player));
 	addPlayerToTile(player, tile);
 }
 
-function checkForMovement() {
-	$(document).keyup(function(e){
+const checkForMovement = () => {
+	$(document).keyup((e) => {
 		var direction; 
 
     	if (e.which == 38) {
@@ -178,21 +224,13 @@ function checkForMovement() {
    		if (direction != undefined) {
 	    	movePlayerToTile(playerInfo.id, findTileByDirection(findTileObjectByPlayer(playerInfo.id), direction));
    		}
-	});
-}
-
-function addPlayerToTile(player, tile) {
-	tile.innerHTML = "<div id=\"P_" + player + "\"><p>" + getPlayerInfoByName(player) + "</p></div>";
-}
-
-function removePlayerFromTile(player, tile) {
-	tile[0].innerHTML = "†";
+	})
 }
 
 // ___ HELPER FUNCTIONS ___
 
 
-function checkIfIdExists (id) {
+const checkIfIdExists = (id) => {
 	if ($("#" + id).length) {
 		return true;
 
@@ -201,18 +239,33 @@ function checkIfIdExists (id) {
 	}
 }
 
-function splitIdToArray (id) {
-	return [id.split("_")[0], Number(id.split("_")[1]), Number(id.split("_")[2])];
+const generateElementAndId = (element, id) => {
+	return "<" + element + " id=\"" + id + "\"></" + element + ">";
 }
 
-function editIdString(oldId, a, b) {
-	var array = splitIdToArray(oldId);
-	return array[0] + "_" + (array[1] + a) + "_" + (array[2] + b);
+// ___ ID STRING FUNCTIONS ___
+
+const convertObjectToIdString = (object) => {
+	return generateIdString(object[0].id.split("_")[0], object[0].id.split("_")[1], object[0].id.split("_")[2]);
 }
 
+const splitIdStringToArray = (id) => {
+	return id.split("_");
+}
 
-$('document').ready(function() {
-	generateTiles("table_0_0", 6);
+const editIdString = (oldId, a, b) => {
+	var array = splitIdStringToArray(oldId);
+	return generateIdString(array[0], (Number(array[1]) + a), (Number(array[2]) + b));
+}
+
+const generateIdString = (element, a, b) => {
+	return element + "_" + a + "_" + b
+}
+
+// ___ RUN ___
+
+$('document').ready(() => {
+	generateRows("table_0_0", 7);
 	generatePlayerAtTile(playerInfo.id, $("#tile_2_2")[0]); // <---- TO FIX: ISSUE WHEN MULTIPLE TABLES
 	checkForMovement();
 });
